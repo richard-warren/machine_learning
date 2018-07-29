d372bdd5f7bdc160db7838e8dc2a37540961cc8d
rwarren2163 <richardwarren2163@gmail.com>
2018-07-28 20:13:50 -0400

    giterdone

--- whisker_segmentation/training.py
+++ whisker_segmentation/training.py
@@ -1,18 +1,73 @@
 # WHISKER SEGMENTATION
-
+'''
+to do:
+use all images, and split training, test sets
+pick up training where left off
+checkpoint saving, training termination rules
+data generator?
+try with resnet50 on top
+'''
 
 from utils import create_network
+from keras.optimizers import Adam
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os.path
 
 
-# settings image size
-img_size = (10,10,3)
-filters = 32
-output_channels = 4
+
+# settings
+binary_labels = True
+lr_init = .001
+loss_fcn = 'binary_crossentropy' if binary_labels else 'mean_squared_error'
+batch_size = 32
+total_samples = 5000
+filters = 16
+output_channels = 3
+data_dir = 'data\\frames'
+labels_dir = 'data\\labeled'
+img_dims = (548,640) # currently must be divisible by 4
+dilation = 40
+gauss_blurring = 75 # needs to be odd number
+
+
+# load and augment data
+X = np.empty((total_samples, img_dims[0], img_dims[1], 1), dtype='float32')
+Y = np.empty((total_samples, img_dims[0], img_dims[1], output_channels),
+             dtype='bool' if binary_labels else 'float32')
+dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(dilation,dilation)) # used to thicken the whiskers up when using binary labels
+
+for i in range(total_samples):
+    X[i] = cv2.imread('%s\\img%i.png' % (data_dir, i+1))[0:img_dims[0], 0:img_dims[1], 1, None].astype('float32') / 255
+    print(i/total_samples)
+    for j in range(output_channels):
+        if os.path.isfile("%s\\frame%05d_whisker_C%i.png" % (labels_dir, i+1, j+1)):
+            img = cv2.imread("%s\\frame%05d_whisker_C%i.png" % (labels_dir, i+1, j+1))[0:img_dims[0], 0:img_dims[1], 1]
+            if binary_labels:
+                img = cv2.dilate(img, dilation_kernel)
+            else:
+                img = cv2.GaussianBlur(img.astype('float32'), (gauss_blurring,gauss_blurring),0)
+        else:
+            img = np.zeros((img_dims[0], img_dims[1])) # set confidence map to all zeros if whisker is not in frame
+        Y[i,:,:,j] = img
+
+
+# normalize Y values
+if not binary_labels:
+    Y = Y / np.max(Y)
+
+
+model = create_network(X[0].shape, output_channels, filters, optimizer=Adam(lr=lr_init), loss_fcn=loss_fcn)
+model.fit(X, Y, batch_size=batch_size, epochs=10, verbose=1)
 
 
 
-model = create_network(img_size, output_channels, filters)
 
+
+
+# show prediction
+predictions = model.predict(X[1:10,])
 
 
 
--- whisker_segmentation/utils.py
+++ whisker_segmentation/utils.py
@@ -1,14 +1,13 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D
-from keras.optimizers import Adam
 from keras.utils import Sequence
 
 
 
 
-def create_network(img_size, output_channels, filters=64):
+def create_network(img_size, output_channels, filters=64, optimizer='adam', loss_fcn='mean_squared_error'):
     
-    # define fully convolutional network
+    # create fully convolutional network
 
     x_in = Input(shape=img_size)
 
@@ -26,7 +25,6 @@ def create_network(img_size, output_channels, filters=64):
     x3 = Conv2D(filters*4, kernel_size=3, padding="same", activation="relu")(x3)
     x3 = Conv2D(filters*4, kernel_size=3, padding="same", activation="relu")(x3)
     
-    
     x4 = Conv2DTranspose(filters*2, kernel_size=3, strides=2, padding="same", activation="relu", kernel_initializer="glorot_normal")(x3)
     x4 = Conv2D(filters*2, kernel_size=3, padding="same", activation="relu")(x4)
     x4 = Conv2D(filters*2, kernel_size=3, padding="same", activation="relu")(x4)
@@ -34,20 +32,10 @@ def create_network(img_size, output_channels, filters=64):
     x_out = Conv2DTranspose(output_channels, kernel_size=3, strides=2, padding="same", activation="linear", kernel_initializer="glorot_normal")(x4)
     
     # compile
-    net = Model(inputs=x_in, outputs=x_out, name="LeapCNN")
-    net.compile(optimizer=Adam(), loss="mean_squared_error")
+    net = Model(inputs=x_in, outputs=x_out, name="whiskerTracer")
+    net.compile(optimizer=optimizer, loss=loss_fcn)
     
     # show network summary
     net.summary()
-
-
-
-
-
-
-'''
-class DataGenerator(Sequence):
     
-    def __init__(self, list_IDs, labels, batch_size=8, dim=(32,32,32), n_channels=1, n_classes=10, shuffle=True):
-'''     
-    
+    return net
