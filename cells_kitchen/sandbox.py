fe5f8507dcd2de958efdfa0fda2f601099f3885a
rwarren2163 <richardwarren2163@gmail.com>
2019-05-26 13:44:49 -0400

    wip

--- cells_kitchen/sandbox.py
+++ cells_kitchen/sandbox.py
@@ -1,4 +1,6 @@
+prefix = "F:\\cells_kitchen_files\\images_"
+suffixes = ['N.00.00', 'N.01.01', 'N.02.00', 'N.03.00.t', 'N.04.00.t', 'YST']
 
-folder = "Z:\\RAW\\cells_kitchen_files\\images_YST"
+vid_num = 5
 
-preview_vid(folder, frames_to_show=500, fps=30, close_when_done=True)
+preview_vid(prefix+suffixes[vid_num], frames_to_show=50, fps=100, close_when_done=False, percentiles=(0, 100))
--- cells_kitchen/utils.py
+++ cells_kitchen/utils.py
@@ -15,24 +15,24 @@ def get_frame(file, percentiles=(0, 100)):
     img = Image.open(file)
     img_np = np.array(img, dtype='float32')
     [pix_min, pix_max] = np.percentile(img_np, np.array(percentiles))
-    img_np = 1 - np.clip((img_np-pix_min) / (pix_max-pix_min), 0, 1)  # scale to between 0 and 1
+    img_np = np.clip((img_np-pix_min) / (pix_max-pix_min), 0, 1)  # scale to between 0 and 1
     return img_np
 
 
-def preview_vid(folder, frames_to_show=100, fps=30, close_when_done=False):
+def preview_vid(folder, frames_to_show=100, fps=30, close_when_done=False, percentiles=(0, 100)):
     """
     opens window and plays movie from sequence of .tif files
     shows the first frames_to_show frames contained in folder
     """
 
     # initialize window
-    im_plot = plt.imshow(np.zeros((200, 256), dtype='float32'), cmap='plasma', vmin=0, vmax=1)
+    im_plot = plt.imshow(np.zeros((1, 1), dtype='float32'), cmap='plasma', vmin=0, vmax=1)
     plt.show()
     files = glob.glob(os.path.join(folder, '*.tif'))
     frames_to_show = min(frames_to_show, len(files))
 
     for f in tqdm(files[0:frames_to_show]):
-        frame = get_frame(f)
+        frame = get_frame(f, percentiles)
         im_plot.set_data(frame)
         plt.pause(1/fps)
 
